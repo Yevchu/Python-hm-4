002d2c2cb2d625393cd484028bc13cfb6d53f163
Vitalyi <evciu97@gmail.com>
2023-05-04 00:41:09 +0300

    доробив поставленну задачу

--- main.py
+++ main.py
@@ -11,30 +11,47 @@ HANDLERS = {
     'close': goodbye
 }
 @input_error
-def parse_input(user_input):
-    command, *args = user_input.split()
-    command = command.lstrip()
+# def parse_input(user_input):
+#     command, *args = user_input.strip().split(' ', 1)
 
-    try:
-        handler = HANDLERS[command.lower()]
-    except KeyError:
-        if args:
-            command = command + ' ' + args[0]
-            args = args[1:]
-        handler = HANDLERS.get(command.lower(), unknown_command)
-    return handler, args
+
+
+#     handler = HANDLERS.get(command.lower(), unknown_command)
+    # try:
+    #     handler = HANDLERS[command.lower()]
+    # except KeyError:
+    #     if args:
+    #         command = command + ' ' + args[0]
+    #         args = args[1:]
+    #     handler = HANDLERS.get(command.lower(), unknown_command)
+#     return handler, args
 
 def main():
     while True:
         user_input = input('Please enter command or command and args: ')
-        handler, *args = parse_input(user_input)
-        try:
-            result = handler(*args)
-            if not result:
-                break
+        command, *args = user_input.strip().split(' ', 1)
+
+        if HANDLERS.get(command):
+            handler = HANDLERS.get(command.lower())
+            if args:
+                args = args[0].split(' ')
+        else:
+            if HANDLERS.get(command) is None:
+                command = command + ' ' + args[0]
+                handler = HANDLERS.get(command.lower())
+            handler = HANDLERS.get(command.lower(),unknown_command(command))
+        result = handler(*args)
+        if result is None:
+            break
+        else:
             print(result)
-        except TypeError:
-            print("Give me a command or command and args")
+        # try:
+        #     result = handler(*args)
+        #     if not result:
+        #         break
+        #     print(result)
+        # except TypeError:
+        #     print("Give me a command or command and args")
 
 if __name__ == '__main__':
     main()
--- utility_func.py
+++ utility_func.py
@@ -13,25 +13,24 @@ def input_error(func):
             return 'Enter user name'
     return inner
 
-def hello_user(_):
+def hello_user():
     return 'How can I help you?'
 
-def unknown_command(_):
-    return 'Unknown command'
+def unknown_command(command):
+    return f'Unknown command: {command}'
 
-def goodbye(_):
+def goodbye():
     print('Good bye!')
     return
 
 @input_error
-def add_user(args):
-    name, phone = args
+def add_user(name, phone):
+    # name, phone = args
     CONTACTS[name] = phone
     return f'User {name} added!'
 
 @input_error
-def change_phone(args):
-    name, phone = args
+def change_phone(name, phone):
     old_phone = CONTACTS[name]
     CONTACTS[name] = phone
     return f'{name}`s old phone number: {old_phone} has been changed to a new one: {phone}'
@@ -43,9 +42,9 @@ def show_all(_):
         result += f'Name: {name}, phone: {phone}\n'
     return result
 
-def show_phone(args):
+def show_phone(name):
     result = ''
     for phone in CONTACTS.values():
-        for name in args:
-            result = f'{name} phone number is: {phone}'
+        result = f'{name} phone number is: {phone}'
     return result
+
